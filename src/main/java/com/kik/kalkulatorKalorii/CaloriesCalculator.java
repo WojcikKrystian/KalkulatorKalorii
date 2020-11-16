46f7be667d695f3f50ad6162db721e18276267a6
F4tall <kamil.boksa@gmail.com>
2020-11-16 20:41:44 +0100

    Initial commit

---
+++ src/main/java/com/kik/kalkulatorKalorii/CaloriesCalculator.java
@@ -0,0 +1,13 @@
+package com.kik.kalkulatorKalorii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesCalculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesCalculator.class, args);
+	}
+
+}
---
+++ src/main/java/com/kik/kalkulatorKalorii/MainController.java
@@ -0,0 +1,13 @@
+package com.kik.kalkulatorKalorii;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "index";
+    }
+}
